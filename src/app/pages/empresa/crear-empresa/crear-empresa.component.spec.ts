53f81560ef32b614d39035fe274bdc0ba0601bdb
Hola Daniel Mejía <holadanielmejia@gmail.com>
2023-04-11 13:40:54 -0500

    Resolviendo error

--- src/app/pages/empresa/crear-empresa/crear-empresa.component.spec.ts
+++ src/app/pages/empresa/crear-empresa/crear-empresa.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CrearEmpresaComponent } from './crear-empresa.component';
 
@@ -6,7 +6,7 @@ describe('CrearEmpresaComponent', () => {
   let component: CrearEmpresaComponent;
   let fixture: ComponentFixture<CrearEmpresaComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CrearEmpresaComponent ]
     })
