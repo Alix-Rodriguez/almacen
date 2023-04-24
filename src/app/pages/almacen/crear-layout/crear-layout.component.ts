cc9e7eb2db9ac3a820e68db930dd52fc841c5848
Hola Daniel Mejía <holadanielmejia@gmail.com>
2023-04-24 11:10:18 -0500

    maqueta de layout

--- src/app/pages/almacen/crear-layout/crear-layout.component.ts
+++ src/app/pages/almacen/crear-layout/crear-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-layout',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearLayoutComponent implements OnInit {
 
-  constructor() { }
+  miga:string = "Crear Layout";
+  checkoutForm!: FormGroup;
+
+  constructor(private readonly fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.checkoutForm = this.initForm();
+  }
+
+  initForm(): FormGroup {
+    return this.fb.group({
+      frm_empresa:['',[Validators.required]],
+      frm_titulo:['', [Validators.required]],
+      frm_tipo:['', [Validators.required]],
+      frm_calle:['', [Validators.required]],
+      frm_numeroe:['',[Validators.required]],
+      frm_numeroi:['',[Validators.required]],
+      frm_delegacion:['',[Validators.required]],
+      frm_colonia:['',[Validators.required]],
+      frm_codigo:['',[Validators.required]],
+      frm_telefono:['',[Validators.required]],
+      frm_email:['',[Validators.required]],
+      frm_uso:['',[Validators.required]],
+    })
+ }
+
+  onSubmit(){
+
   }
 
 }
