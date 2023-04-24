e4969f7aeba44571ed0505f58aaf3c30ff321b44
Alix-Rodriguez <alixm.081@gmail.com>
2023-04-24 19:12:58 -0400

    catalogo marca producto tabla añadida

--- src/app/pages/catalogo/catalogo-marca-producto/catalogo-marca-producto.component.ts
+++ src/app/pages/catalogo/catalogo-marca-producto/catalogo-marca-producto.component.ts
@@ -1,17 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'app-catalogo-marca-producto',
-  templateUrl: './catalogo-marca-producto.component.html',
-  styleUrls: ['./catalogo-marca-producto.component.css']
+  selector: "app-catalogo-marca-producto",
+  templateUrl: "./catalogo-marca-producto.component.html",
+  styleUrls: ["./catalogo-marca-producto.component.css"],
 })
 export class CatalogoMarcaProductoComponent implements OnInit {
+  miga: any = "Catalogo Marca Producto";
 
-  miga: any = 'Catalogo Marca Producto';
+  constructor(private modalService: NgbModal) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  // MODAL
+
+  closeResult: string = "";
+
+  open(content: any) {
+    this.modalService
+      .open(content, { ariaLabelledBy: "modal-basic-title" })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
   }
 
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return "by pressing ESC";
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return "by clicking on a backdrop";
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
