401fdee6d3f8f51c70eba0983cd45a0473cbdbd5
Alix-Rodriguez <alixm.081@gmail.com>
2023-04-25 17:56:23 -0400

    actualizacion

--- src/app/pages/catalogo/catalogo-cliente/catalogo-cliente.component.ts
+++ src/app/pages/catalogo/catalogo-cliente/catalogo-cliente.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { DataCatalogoService } from "../../../services/datacatalogo.service";
+import { DataserviceService } from "../../../services/dataservice.service";
 
 @Component({
-  selector: 'app-catalogo-cliente',
-  templateUrl: './catalogo-cliente.component.html',
-  styleUrls: ['./catalogo-cliente.component.css']
+  selector: "app-catalogo-cliente",
+  templateUrl: "./catalogo-cliente.component.html",
+  styleUrls: ["./catalogo-cliente.component.css"],
 })
 export class CatalogoClienteComponent implements OnInit {
-  miga: any = 'Catalogo De Clientes';
+  miga: any = "Catalogo De Clientes";
+  checkoutForm!: FormGroup;
+  empresa: any;
 
-  constructor() { }
+  constructor(
+    private readonly fb: FormBuilder,
+    private dataCatalogo: DataCatalogoService,
+    private dataService: DataserviceService
+  ) {}
+
+    ListarEmpresa(){
+    this.dataService.getListEmpresa()
+    .subscribe(resp=>{
+      this.empresa = resp['data'];
+      console.log(this.empresa)
+    })
+    }
 
   ngOnInit(): void {
+    this.ListarEmpresa()
   }
-
 }
---
+++ src/app/services/datacatalogo.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataCatalogoService {
+  url = "http://127.0.0.1:8000/api/";
+
+  constructor(private http:HttpClient) { }
+
+  SaveCliente(value){
+    return this.http.post(this.url + 'save-cliente', value)
+  }
+
+
+}
