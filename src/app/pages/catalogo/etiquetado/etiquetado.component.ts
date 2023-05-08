532c51d7a11e88d6677632c18741dde9be70f57d
Alix-Rodriguez <alixm.081@gmail.com>
2023-05-08 17:41:19 -0400

    Modificacion en vista precia de etiquetado y iconos de (-) en crear almacen

--- src/app/pages/catalogo/etiquetado/etiquetado.component.ts
+++ src/app/pages/catalogo/etiquetado/etiquetado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-etiquetado',
@@ -6,11 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./etiquetado.component.css']
 })
 export class EtiquetadoComponent implements OnInit {
-  miga:string = "Etiquetado";
+  miga: string = "Etiquetado";
+  public archivos: any = []
+  public previsualizacion: string;
+  Nombre_empresa:string;
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) { }
+
+
 
   ngOnInit(): void {
   }
 
+  capturarFile(event): any {
+    const archivoCapturado = event.target.files[0]
+    this.extraerBase64(archivoCapturado).then((imagen:any)=>{
+      this.previsualizacion = imagen.base;
+      console.log(imagen);
+    })
+    this.archivos.push(archivoCapturado)
+  }
+
+
+  // transformacion de la imagen para visualizar
+  extraerBase64 = async ($event: any) => new Promise((resolve, reject) => {
+    try {
+      const unsafeImg = window.URL.createObjectURL($event);
+      const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
+      const reader = new FileReader();
+      reader.readAsDataURL($event);
+      reader.onload = () => {
+        resolve({
+          base: reader.result
+        });
+      };
+      reader.onerror = error => {
+        resolve({
+          base: null
+        });
+      };
+
+    } catch (e) {
+      return null;
+    }
+  })
 }
